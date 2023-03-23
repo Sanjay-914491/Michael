856174177f1a47d166ba5015dae854501c9b449f
Praveentech21 <ravikuar_csd@srkrec.edu.in>
2023-03-24 00:58:36 +0530

    php changes shiva

---
+++ index1.php
@@ -0,0 +1,44 @@
+<?php
+include "header.php"
+?>
+
+
+<div id="header-carousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="w-100" src="img/carousel-1.png" alt="Image">
+                    <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                        <div class="p-3" style="max-width: 900px;">
+                            <h5 class="text-white text-uppercase mb-3 animated slideInDown">Subscription based</h5>
+                            <h1 class="display-1 text-white mb-md-4 animated zoomIn">Pickup and Drop Delivery Service</h1>
+                            
+                        </div>
+                    </div>
+                </div>
+                <!-- Navbar & Carousel End -->
+            <div class="carousel-item">
+                    <img class="w-100" src="img/carousel-2.png" alt="Image">
+                    <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                        <div class="p-3" style="max-width: 900px;">
+                            <h5 class="text-white text-uppercase mb-3 animated slideInDown">Subscription based</h5>
+                            <h1 class="display-1 text-white mb-md-4 animated zoomIn">Pickup and Drop Delivery Service</h1>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#header-carousel"
+                data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#header-carousel"
+                data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+
+<?php
+include "footer.php"
+?>
